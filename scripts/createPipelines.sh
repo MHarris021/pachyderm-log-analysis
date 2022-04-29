54d01c6dd09a3e4edd465a2fcaf874db1ad8549d
Michael Haris <mharris021@hotmail.com>
2022-04-29 12:13:28 -0500

    switched analysis pipeline output to json format from text
    - added the ability to specify image version
    - added image version parameter to scripts
    - next step is to aggregate data from log analysis
    
    Signed-off-by: Michael Harris <mharris@darcstar-solutions.tech>

--- scripts/createPipelines.sh
+++ scripts/createPipelines.sh
@@ -3,3 +3,4 @@ set -x
 version=$1
 pachctl create pipeline --jsonnet ./pipelines/log-analyzer.jsonnet --arg suffix="warning" --arg phrase="warning" --arg imageVersion="$version"
 pachctl create pipeline --jsonnet ./pipelines/log-analyzer.jsonnet --arg suffix="error" --arg phrase="error" --arg imageVersion="$version"
+pachctl create pipeline --jsonnet ./pipelines/log-analyzer.jsonnet --arg suffix="critical" --arg phrase="critical" --arg imageVersion="$version"
